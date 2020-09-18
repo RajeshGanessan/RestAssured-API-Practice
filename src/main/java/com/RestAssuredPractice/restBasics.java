a9ae7ea4b08e3cf44de4e2896e81ee59e518a1b2
rajesh g <rajeshg.gmx@gmail.com>
2020-09-18 14:18:44 +0530

    Added restAssured Basic Requests

--- src/main/java/com/RestAssuredPractice/restBasics.java
+++ src/main/java/com/RestAssuredPractice/restBasics.java
@@ -1,33 +1,55 @@
 package com.RestAssuredPractice;
 
+import com.RestAssuredPractice.Data.PayloadJson;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.json.Json;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class restBasics {
 
     public static void main(String[] args) {
+        JsonPath json;
 
         RestAssured.baseURI="https://rahulshettyacademy.com/";
 
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+      String AddPlaceResponse =  given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body(PayloadJson.getAddPlace())
+                .when().post("/maps/api/place/add/json")
+                .then().assertThat().statusCode(200).body("scope",equalTo("APP"))
+                .header("server","Apache/2.4.18 (Ubuntu)").extract().response().asString();
+
+        System.out.println(AddPlaceResponse);
+
+         json = new JsonPath(AddPlaceResponse);  // for parsing String as a json
+         String place_id = json.get("place_id");
+
+
+         //Update Place API
+       String UpdateReponse =  given().queryParam("key","qaclick123").header("Content-Type","application/json")
                 .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
+                        "\"place_id\":\""+ place_id+"\",\n" +
+                        "\"address\":\"15th cross richmond road, Bangalore\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
                         "}\n" +
                         " \n")
-                .when().post("/maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200);
+                .when().put("/maps/api/place/update/json")
+                .then().assertThat().statusCode(200).extract().response().asString();
+            JsonPath jsonUpdate = new JsonPath(UpdateReponse);
+            String UpdateSucess = jsonUpdate.get("msg");
+        System.out.println(UpdateSucess);
+
+
+        //Get the Updated Place
+       String getPlace =  given().queryParam("key","qaclick123").queryParam("place_id",place_id)
+                .header("Content-Type","application/json")
+                .when().get("/maps/api/place/get/json")
+                .then().assertThat().statusCode(200).extract().response().asString();
+        System.out.println("Get Place = " + getPlace);
+
+
+
     }
 }
